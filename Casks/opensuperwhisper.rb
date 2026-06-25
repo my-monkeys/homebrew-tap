df7d4a6fcf297cee3662bf128f18724631066cc2
MaximCosta <91082344+MaximCosta@users.noreply.github.com>
2026-06-25 15:35:20 +0200

    opensuperwhisper 0.9.0

--- Casks/opensuperwhisper.rb
+++ Casks/opensuperwhisper.rb
@@ -1,13 +1,13 @@
 cask "opensuperwhisper" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.8.0"
+  version "0.9.0"
 
   on_arm do
-    sha256 "137f96a4832137661705095d884a73917e1a44420403e1dd1a323174debee5e6"
+    sha256 "56047d8a8b8534dd4f9009fc2619d87c04a0185148dc0e10f2f0a75aed856ae4"
   end
   on_intel do
-    sha256 "b84fc1c0985fee48ce5d3df0a6b87ad823fde95c32c5b9daed78dfea38ab2c42"
+    sha256 "a40026d7413f500f9c991b9893fbbd4081d8873d8fe19a16d7532aa251918a43"
   end
 
   url "https://github.com/my-monkeys/OpenSuperWhisper/releases/download/v#{version}/OpenSuperWhisper-#{arch}-#{version}.dmg",
