42f4320d0a2f88d55105186f1bc27438ca6cff61
Maxio <91082344+MaximCosta@users.noreply.github.com>
2026-07-12 21:07:01 +0200

    Add glance cask 1.0.0

---
+++ Casks/glance.rb
@@ -0,0 +1,21 @@
+cask "glance" do
+  version "1.0.0"
+  sha256 "0315d41c1aa66ac66f31ba345d28fd539e5077e2a64a93dab5ce25764adf3e7a"
+
+  url "https://github.com/my-monkeys/glance/releases/download/v#{version}/Glance-#{version}.dmg",
+      verified: "github.com/my-monkeys/glance/"
+  name "Glance"
+  desc "Mobile & desktop client for Umami and Plausible analytics"
+  homepage "https://github.com/my-monkeys/glance"
+
+  depends_on macos: ">= :monterey"
+
+  app "Glance.app"
+
+  zap trash: [
+    "~/Library/Application Support/fr.mymonkey.glance",
+    "~/Library/Caches/fr.mymonkey.glance",
+    "~/Library/HTTPStorages/fr.mymonkey.glance",
+    "~/Library/Preferences/fr.mymonkey.glance.plist",
+  ]
+end
