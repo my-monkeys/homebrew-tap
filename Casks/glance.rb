3d4d33c472a636ca4e07e7db3f49972e65c2e667
MaximCosta <noreply@my-monkey.fr>
2026-07-17 17:55:35 +0200

    glance 1.3.0

--- Casks/glance.rb
+++ Casks/glance.rb
@@ -1,6 +1,6 @@
 cask "glance" do
-  version "1.2.0"
-  sha256 "8b6e7e094783e94711e7484422359351924290860424eddda6ee661a84d352b4"
+  version "1.3.0"
+  sha256 "28fa095ccff60ff5f542a2887d616073b2d06135468f0040ca2430bfcc7e4b83"
 
   url "https://github.com/my-monkeys/glance/releases/download/v#{version}/Glance-#{version}.dmg",
       verified: "github.com/my-monkeys/glance/"
