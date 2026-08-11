8d12d072223fbf0a74d50b06f72886ead438313b
Maxio <91082344+MaximCosta@users.noreply.github.com>
2026-08-11 02:20:28 +0200

    glance 1.7.0

--- Casks/glance.rb
+++ Casks/glance.rb
@@ -1,6 +1,6 @@
 cask "glance" do
-  version "1.6.0"
-  sha256 "01ddc6eef48471aa7cf5e0f82fc2fb4bb0d3c1179cba0dca9b96761e5624045f"
+  version "1.7.0"
+  sha256 "4867a63d0b2b4756cfd1282048a529505e5af9025e12164dd5ac12385815adce"
 
   url "https://github.com/my-monkeys/glance/releases/download/v#{version}/Glance-#{version}.dmg",
       verified: "github.com/my-monkeys/glance/"
