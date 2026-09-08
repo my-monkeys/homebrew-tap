286f9b9a5ae6443f0e5b38297f0b5db11d181c88
Alan Wong <3414995+alanwonglah@users.noreply.github.com>
2026-09-09 00:54:14 +0800

    opensuperwhisper 0.12.2 (#1)
    * opensuperwhisper 0.12.2
    
    * fix: Calling the `verified` parameter in the `url` stanza is deprecated

--- Casks/opensuperwhisper.rb
+++ Casks/opensuperwhisper.rb
@@ -1,17 +1,16 @@
 cask "opensuperwhisper" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.9.9"
+  version "0.12.2"
 
   on_arm do
-    sha256 "f3e5a399f36fbbf13e952212d569e4d0e5c72299fb9d6935586b7a7556659d34"
+    sha256 "2587fae040357876bc5a0d1a6797dbe5396169ad821e77d22bdfa2ae76db65f8"
   end
   on_intel do
-    sha256 "501dbace8b1f0e0e3704f261e80730b9d352a27048b510334fa74677865b4d58"
+    sha256 "ecdf3a34454caa6694bd2106e382bdbad4e5f41168d25bb84c6ad6a26e43a5dd"
   end
 
-  url "https://github.com/my-monkeys/OpenSuperWhisper/releases/download/v#{version}/OpenSuperWhisper-#{arch}-#{version}.dmg",
-      verified: "github.com/my-monkeys/OpenSuperWhisper/"
+  url "https://github.com/my-monkeys/OpenSuperWhisper/releases/download/v#{version}/OpenSuperWhisper-#{arch}-#{version}.dmg"
   name "OpenSuperWhisper"
   desc "macOS dictation with local Whisper/Parakeet transcription"
   homepage "https://github.com/my-monkeys/OpenSuperWhisper"
